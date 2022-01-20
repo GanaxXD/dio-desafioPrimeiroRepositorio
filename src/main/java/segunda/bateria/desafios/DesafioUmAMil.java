8c7e65a9060c2494d2f62c6a74adca0ef9adf66b
GanaxXD <pedro.mattis.df@gmail.com>
2022-01-20 20:04:30 -0300

    entrega da segunda bateria de testes da DIO - 3 starter GFT

---
+++ src/main/java/segunda/bateria/desafios/DesafioUmAMil.java
@@ -0,0 +1,18 @@
+/*
+package segunda.bateria.desafios;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DesafioUmAMil {
+
+        public static void main(String[] args) throws IOException {
+            Scanner leitor = new Scanner(System.in);
+            int N = leitor.nextInt();
+            for (int i = 0 ; i < 10000 ; i++       ) {
+                if ((i%N) == 2) System.out.println(   i   );
+            }
+        }
+//Complete com a sua lógica nos espaços em branco
+    }
+}
+*/
---
+++ src/main/java/segunda/bateria/desafios/Xadrez.java
@@ -0,0 +1,31 @@
+/*
+package segunda.bateria.desafios;
+import java.util.Scanner;
+
+public class Xadrez {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int L = sc.nextInt();
+        int C = sc.nextInt();
+        
+        //Verificando se as condições de entrada são verdadeiras
+		*/
+/*if(L<1 || L>1000 || C<1 || C>1000)
+		  System.out.println("Sua entrada fere a regra. A linha e a coluna não devem ser maiores que 1000, nem menores que 1.")
+		*//*
+
+        
+        //imprimindo o resultado
+        if (( L%2==0)&&(C%2!=0))
+            System.out.println(" 0  ");
+        else if (( L%2==0)&&(C%2==0))
+            System.out.println(" 1  ");
+        else if ((L%2!=0) && (C%2!=0))                                              //complete o código nos espaços em branco
+            System.out.println("  1 ");
+        else if ((L%2!=0)&&(C%2==0))
+            System.out.println("  0 ");
+        sc.close();
+    }
+}
+*/
